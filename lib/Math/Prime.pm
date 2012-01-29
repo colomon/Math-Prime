498c3ced3b0ce67b3f37f6753d71955dc9b0ada7
Solomon Foster <colomon@gmail.com>
2012-01-28 19:33:11 -0500

    Remove (:DEFAULT) to see if that makes niecza happy.

--- lib/Math/Prime.pm
+++ lib/Math/Prime.pm
@@ -26,11 +26,11 @@ my @primes := (2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 6
                409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491,
                499, 503, 509, 521, 523, 541, &next-prime ... *);
 
-sub primes() is export(:DEFAULT) {
+sub primes() is export {
     @primes.iterator.list;
 }
 
-sub is-prime(Int $x) is export(:DEFAULT) {
+sub is-prime(Int $x) is export {
     return False if $x == 1;
     my $limit = $x.sqrt.Int;
     for primes() -> $p {
